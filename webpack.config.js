91e456b7f58f48fc5e76d060048e1223abd72c3e
Michael Kropat <michael.kropat@rackspace.com>
2018-02-23 09:59:06 -0500

    Optimize bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,18 @@
 const path = require('path');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const webpack = require('webpack');
 
 module.exports = {
   entry: './index.web.js',
   output: {
-    filename: 'secure-random-password.js',
+    filename: 'secure-random-password.min.js',
     path: path.resolve(__dirname, 'dist')
   },
+  externals: {
+    './../../process/browser.js': '{}',
+    './../../buffer/index.js': '{}',
+  },
+  plugins: [
+    new UglifyJsPlugin(),
+  ],
 };
