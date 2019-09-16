70161b97c548ee61ef6e2a43726baed5d9918e6f
Michael Kropat <mail@michael.kropat.name>
2019-09-16 08:34:51 -0400

    Update to Webpack 4

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,11 @@
-const path = require('path');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
-const webpack = require('webpack');
-
 module.exports = {
+  mode: 'production',
   entry: './index.web.js',
   output: {
     filename: 'secure-random-password.min.js',
-    path: path.resolve(__dirname, 'dist')
   },
   externals: {
     './../../process/browser.js': '{}',
     './../../buffer/index.js': '{}',
   },
-  plugins: [
-    new UglifyJsPlugin(),
-  ],
 };
