882671369c2ce86f189bf94aeb265b6c2dc398b1
Peter Staples <staplespeter.ni@gmail.com>
2023-05-24 13:56:50 +0100

    Added type declarations for ESM support

---
+++ lib/util.d.ts
@@ -0,0 +1,8 @@
+export function assign(...args: any[]): any;
+export function intersection(left: any, right: any): string[];
+export function isInteger(n: any): boolean;
+export function isString(s: any): boolean;
+export function range(n: any): any;
+export function repeat(val: any, n: any): any;
+export function toArray(x: any): any;
+//# sourceMappingURL=util.d.ts.map
